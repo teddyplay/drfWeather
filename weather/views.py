40468cd176c446d7241f37aeb57f303a2003dbe1
teddyplay <kasymovablai8@gmail.com>
2022-11-06 15:26:18 +0600

    написал класс для представления модели  Stars

--- weather/views.py
+++ weather/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics #базовый класс для предсавленя drf
+
+from weather.models import Stars
+
+
+class StarsAPIView(generics.ListAPIView):
+    queryset = Stars.objects.all() # будет брать все поля модели
+    serializer_class = StarsSerializers()
+
