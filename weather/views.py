af2b24306183ac06da06897f1589b624c98cc812
teddyplay <kasymovablai8@gmail.com>
2022-11-06 16:10:48 +0600

    написал базовое предсатавлние для views.py

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics #базовый класс для предсавленя drf
+from rest_framework.response import Response
 
 from weather.models import Stars
 from weather.serializers import StarsSerializer
+from rest_framework.views import APIView
 
 
-class StarsAPIView(generics.ListAPIView):
-    queryset = Stars.objects.all() # будет брать все поля модели
-    serializer_class = StarsSerializer
+
+
+class StarsAPIView(APIView):
+    def get(self, request):
+        return Response({'title':'Jason'})   # Возращать клиенту json стороку
+
+# class StarsAPIView(generics.ListAPIView):
+#     queryset = Stars.objects.all() # будет брать все поля модели
+#     serializer_class = StarsSerializer
 
