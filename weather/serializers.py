70e0405152be38d6eaf8195aa004fb872e9fbae9
teddyplay <kasymovablai8@gmail.com>
2022-11-08 21:09:05 +0600

    сделал фуйнцию  для вывода сырых данных для наглядности

--- weather/serializers.py
+++ weather/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 
 from .models import Stars
 
@@ -12,6 +13,20 @@ class StarsModel:
 
 
 class StarsSerializer(serializers.Serializer): # будем брать из бд модели
+    title = serializers.CharField(max_length=350)
+    content = serializers.CharField()
+
+
+def encode():
+    model = StarsModel('Rick and Morty', 'content: rick and morty')
+    model_sr = StarsSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)
+    # фунция  для вывода сыррого формата json
+    print(json)
+
+
+
 
 
 
